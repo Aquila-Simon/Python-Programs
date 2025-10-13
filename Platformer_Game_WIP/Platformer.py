3dbc943566de5b72ecd109457a7d9227eb35cbdb
Simon Villanueva <aquilasimon21@gmail.com>
2025-10-13 03:28:03 -0600

    First Attempt at a Platformer while following a tutorial

---
+++ Platformer_Game_WIP/Platformer.py
@@ -0,0 +1,62 @@
+import pygame, sys
+
+clock = pygame.time.Clock()
+
+from pygame.locals import *
+pygame.init()
+
+WINDOW_SIZE = (400, 400)
+screen = pygame.display.set_mode(WINDOW_SIZE, 0 , 32)
+player_image = pygame.image.load('Python-Programs/Platformer_Game_WIP/player.jpg')
+pygame.display.set_caption('Platformer Game')
+
+moving_left = False
+moving_right = False
+
+player_location = [50, 50]
+player_y_momentum = 0
+
+player_rect = pygame.Rect(player_location[0], player_location[1], player_image.get_width(), player_image.get_height())
+testing_rect = pygame.Rect(100, 100, 100, 50)
+Other_rect = pygame.rect()
+
+while True:
+    screen.fill((138, 154, 91))
+    screen.blit(player_image, player_location)
+
+    if player_location[1] > WINDOW_SIZE[1]-player_image.get_height():
+        player_y_momentum = -player_y_momentum
+    else:
+        player_y_momentum += 0.2
+    player_location[1] += player_y_momentum
+
+    if moving_left == True:
+        player_location[0] -= 4
+    if moving_right == True:
+        player_location[0] += 4
+
+    player_rect.x = player_location[0]
+    player_rect.y = player_location[1]
+
+    if player_rect.colliderect(testing_rect):
+        pygame.draw.rect(screen, (255, 0, 0), testing_rect)
+    else:
+        pygame.draw.rect(screen, (255,255,255), testing_rect)
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == KEYDOWN:
+            if event.key == K_LEFT:
+                moving_left = True
+            if event.key == K_RIGHT:
+                moving_right = True
+        if event.type == KEYUP:
+            if event.key == K_LEFT:
+                moving_left = False
+            if event.key == K_RIGHT:
+                moving_right = False
+
+    pygame.display.update()
+    clock.tick(60)
