5e2facdbadfe01efc7909e97622c47866126ffe3
Simon Villanueva <aquilasimon21@gmail.com>
2025-10-12 21:53:55 -0600

    Adding in Integers Practice to Database

---
+++ Practice_Programs/Integers_Practice.py
@@ -0,0 +1,37 @@
+#Adding practice
+Add: int = 2 + 3
+print(Add)
+Add: int = 4 + 4
+print(Add)
+#Subtracting practice
+Subtract: int = 2 - 3
+print(Subtract)
+Subtract: int = 50 - 25
+print(Subtract)
+#Float Practice
+Decimals: float = 5.25 + 7.55
+print(Decimals)
+Decimals = 4/2
+print(Decimals)
+#When dividing any two integers, it will always result in a float datatype even if it is a whole number
+#Underscore Behavior in code
+Universe_Age = 14_000_000_000 #Underscores are hidden when code is ran
+print(Universe_Age)
+#Multiple Variable Assignment to allow for cleaner code
+x, y, z = 15,30,50
+print(x, y, z)
+#For Constant values that never change, ALWAYS NAME IN ALL CAPS
+MAX_CONNECTION = 50000
+print(MAX_CONNECTION)
+
+#Book Practice
+#2-9 Use all operations to produce the answer of 8 just using the print function
+print(5+3)
+print(10-2)
+print(int(16/2)) #Force result to be an Int
+print(16/2) #Allow it to be a Float
+print(4*2)
+
+#2-10 Assign your favourite number to a variable and print a message presenting your favourite number
+fav_number = 21
+print(f'Hello User! Fun fact, my favourite number is {fav_number}')
