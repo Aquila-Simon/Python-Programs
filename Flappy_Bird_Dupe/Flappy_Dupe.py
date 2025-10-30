d903e6f6eaf57c4713cfd1ce7811a2a9a821f1cb
Simon Villanueva <aquilasimon21@gmail.com>
2025-10-29 21:32:54 -0600

    Added pipe height randomization

--- Flappy_Bird_Dupe/Flappy_Dupe.py
+++ Flappy_Bird_Dupe/Flappy_Dupe.py
@@ -30,14 +30,15 @@ Moving_Left = False
 Moving_Up = False
 Moving_Down = False
 air_timer = 0
-upper_pipe_location = [700, 0]
-lower_pipe_location = [700, 500]
+random_pipe_placement = random.randint(-150, 150)
+upper_pipe_location = [700, random_pipe_placement]
+lower_pipe_location = [700, (random_pipe_placement + 500)]
 
 player_image = pygame.image.load('stephen.png').convert() #Player Image (Placeholder for now)
 player_image.set_colorkey((255,255,255))
 player_rect = pygame.Rect(player_location[0], player_location[1], player_image.get_width(), player_image.get_height()) #Player Rectangle
-Upper_pipe_rect = pygame.Rect(upper_pipe_location[0], upper_pipe_location[1], 100, 300)
-Lower_pipe_rect = pygame.Rect(lower_pipe_location[0], lower_pipe_location[1], 100, 300)
+#Upper_pipe_rect = pygame.Rect(upper_pipe_location[0], upper_pipe_location[1], 100, 300)
+#Lower_pipe_rect = pygame.Rect(lower_pipe_location[0], lower_pipe_location[1], 100, 300)
 
 #Start Sequence for Game
 
@@ -56,13 +57,15 @@ while start_game == True: #Main Game Loop
                 game_state = 'Pause_Game'
             if event.key == K_RETURN and game_state == 'Pause_Game': #Unpause Game
                 game_state = 'Playing_Game'
+            if event.key == K_RETURN and game_state == 'Game_Over': #Restart Game
+                game_state = 'Playing_Game'
 
 
     if game_state == 'Playing_Game':
         display.fill((135, 206, 235)) #Fills the screen with sky blue before drawing elements (Main Background)
         pygame.draw.rect(display, (65,152,10), ground_rect) #Draws the ground
         display.blit(score_text, (5,10)) #Draws Score
-        display.blit(high_score_text, (625,10)) #Draws High Score
+        display.blit(high_score_text, (600,10)) #Draws High Score
         Upper_pipe_rect = pygame.Rect(upper_pipe_location[0], upper_pipe_location[1], 100, 300)
         Lower_pipe_rect = pygame.Rect(lower_pipe_location[0], lower_pipe_location[1], 100, 300)
 
@@ -80,22 +83,20 @@ while start_game == True: #Main Game Loop
             player_location[1] = 300
             upper_pipe_location[0] = 800
             lower_pipe_location[0] = 800
+            upper_pipe_location[1] = random_pipe_placement
             score = 0
             score_text= font.render('Score: ' + str(score), False, (255,255,255))
+            game_state = 'Game_Over'
         if player_rect.colliderect(Lower_pipe_rect): #Lower Pipe Collision
+            random_pipe_placement = random.randint(-150, 250)
             player_location[0] = 100
             player_location[1] = 300
             upper_pipe_location[0] = 800
             lower_pipe_location[0] = 800
+            upper_pipe_location[1] = random_pipe_placement
             score = 0
             score_text= font.render('Score: ' + str(score), False, (255,255,255))
-        if player_rect.colliderect(Lower_pipe_rect): #Lower Pipe Collision
-            player_location[0] = 100
-            player_location[1] = 300
-            upper_pipe_location[0] = 800
-            lower_pipe_location[0] = 800
-            score = 0
-            score_text= font.render('Score: ' + str(score), False, (255,255,255))
+            game_state = 'Game_Over'
 
         if player_rect.colliderect(ground_rect): #Ground Collision
             player_location[0] = 100
@@ -104,6 +105,7 @@ while start_game == True: #Main Game Loop
             score_text= font.render('Score: ' + str(score), False, (255,255,255))
             upper_pipe_location[0] = 800
             lower_pipe_location[0] = 800
+            game_state = 'Game_Over'
 
 
         player_location[1] += gravity
@@ -114,17 +116,20 @@ while start_game == True: #Main Game Loop
         display.blit(player_image, player_location) #Draws the player image
         player_rect.x = player_location[0]
         player_rect.y = player_location[1]
-        upper_pipe_location[0] -= 5
-        lower_pipe_location[0] -= 5
+        upper_pipe_location[0] -= 7
+        lower_pipe_location[0] -= 7
 
         display.blit(score_text, (5,10)) #Draws Score
-        display.blit(high_score_text, (625,10)) #Draws High Score
+        display.blit(high_score_text, (600,10)) #Draws High Score
 
         if (upper_pipe_location[0] + 100) < player_location[0]: #Score Increase and Pipe Reset
+            random_pipe_placement = random.randint(-150, 150)
             score += 1
             score_text= font.render('Score: ' + str(score), False, (255,255,255))
             upper_pipe_location[0] = 800
             lower_pipe_location[0] = 800
+            upper_pipe_location[1] = random_pipe_placement
+            lower_pipe_location[1] = (random_pipe_placement + 500)
         if score > high_score:
             high_score = score
             high_score_text= font.render('High Score: ' + str(high_score), False, (255,255,255))
@@ -150,3 +155,12 @@ while start_game == True: #Main Game Loop
         screen.blit(pygame.transform.scale(display, WINDOW_SIZE), (0,0))
         pygame.display.update()
         clock.tick(60) #sets framerate to 60 fps
+    elif game_state == 'Game_Over':
+        display.fill((0,0,0)) #Fills the screen with black
+        game_over_font = pygame.font.SysFont('Rockwell', 50) #Font for Game Over Text
+        game_over_text_1 = game_over_font.render("Game Over", False, (255, 255, 255)) # White text
+        game_over_text_2 = game_over_font.render("Press RETURN to Restart", False, (255, 255, 255)) # White text
+        display.blit(game_over_text_1, (240, 340)) # Position the text in the center
+        display.blit(game_over_text_2, (110, 410)) # Position the text in the center
+        screen.blit(pygame.transform.scale(display, WINDOW_SIZE), (0,0))
+        pygame.display.update()
